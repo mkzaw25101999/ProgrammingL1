8c875d2068fb46495a5b140577165ab492247e52
Tommy <khantzawkz23@gmail.com>
2024-10-04 17:50:00 +0300

    Add Java Application file for W40 exercises e06

--- w40/e06/App.java
+++ w40/e06/App.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(final String[] args){
-        
+    public static void main(final String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a text:");
+        String originTxt = scn.nextLine();
+
+        String txt = originTxt.toLowerCase().replaceAll("[^a-z]", "");
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = txt.length() - 1;
+        while (start < end) {
+            if (txt.charAt(start) != txt.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(originTxt + " is a palindrome.");
+        } else {
+            System.out.println(originTxt + " is not a palindrome.");
+        }
+        scn.close();
     }
 }
